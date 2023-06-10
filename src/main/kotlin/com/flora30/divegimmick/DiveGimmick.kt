9b7f4e6a2a723a74174d8a9fa95d5e07ba861675
ふろーら <73652094+Flora300@users.noreply.github.com>
2023-06-10 22:31:50 +0900

    kotlin + shadowJar

--- src/main/kotlin/com/flora30/divegimmick/DiveGimmick.kt
+++ src/main/kotlin/com/flora30/divegimmick/DiveGimmick.kt
@@ -1,17 +1,13 @@
-package com.flora30.divegimmick;
+package com.flora30.divegimmick
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin
 
-public final class DiveGimmick extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
+class DiveGimmick : JavaPlugin() {
+    override fun onEnable() {
         // Plugin startup logic
-
     }
 
-    @Override
-    public void onDisable() {
+    override fun onDisable() {
         // Plugin shutdown logic
     }
 }
